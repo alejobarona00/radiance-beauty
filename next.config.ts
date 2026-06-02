c8b44119086d734734944fd05c426b62965ffaac
Alejandro Barona <alejobarona00@gmail.com>
2026-06-01 23:07:44 -0500

    Ajuste para despliegue automático en Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
+  // Eliminamos la línea: output: "export",
   images: {
     unoptimized: true,
   },
