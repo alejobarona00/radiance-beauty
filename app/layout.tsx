ad464788413e0aff0ed567ad0d96082a7f89728d
Alejandro Barona <alejobarona00@gmail.com>
2026-06-06 12:12:26 -0500

    Instalación de Pixel de Meta

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
+import Script from "next/script"; // Importamos el componente Script
 import "./globals.css";
 
 const playfair = Playfair_Display({
@@ -31,7 +32,36 @@ export default function RootLayout({
       lang="es"
       className={`${playfair.variable} ${inter.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col bg-ivory">{children}</body>
+      <head>
+        {/* Aquí insertamos el Píxel de Meta correctamente */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '666994759187050');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
+      <body className="min-h-full flex flex-col bg-ivory">
+        {children}
+        {/* En caso de que Meta necesite validar el Pixel sin JS */}
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=666994759187050&ev=PageView&noscript=1"
+            alt="pixel"
+          />
+        </noscript>
+      </body>
     </html>
   );
 }
